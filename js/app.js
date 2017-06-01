d0e92151c72f8ac4d8987b47df6c21e2742b903f
Meryl Turner <merylturner08@gmail.com>
2017-06-01 16:36:01 -0700

    fixed eslint problems

--- js/app.js
+++ js/app.js
@@ -1,39 +1,37 @@
 'use strict';
 
-//TODO empty array
 var allProjects = [];
 
-//TODO constructor function of projects
+
 function Project(projectDataObj) {
     this.name = projectDataObj.name;
     this.date = projectDataObj.date;
     this.description = projectDataObj.description;
     this.filepath = 'imgs/' + projectDataObj.filepath;
 }
-//TODO prototype function that will add data to the DOM with .toHtml()
+
+
 Project.prototype.toHtml = function () {
-    //TODO clone example markup for each project, using jQuery
     var $newProject = $('article.template').clone();
     $newProject.removeClass('template');
 
-    //create variable grabbing the selector within the main section
     $newProject.find('img').attr('src', this.filepath);
     $newProject.find('h2').html(this.name);
     $newProject.find('h4').html(this.date);
     $newProject.find('p').html(this.description);
-    
     return $newProject;
 };
 
-//push each new project to the allProjects array
+
 projectData.forEach(function (projectDataObj) {
     allProjects.push(new Project(projectDataObj));
 });
 
-//projectData.forEach - append items to the DOM via toHtml
+
 allProjects.forEach(function (project) {
     $('#projects').append(project.toHtml());
 });
-//hide template article
+
+
 $('.template').hide();
 
