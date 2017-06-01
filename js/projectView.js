de917792eda12e1c3eecd5cf833efc85dad15cba
Meryl Turner <merylturner08@gmail.com>
2017-06-01 16:14:54 -0700

    added read on show less functionality to project articles

--- js/projectView.js
+++ js/projectView.js
@@ -4,7 +4,6 @@ var projectView = {};
 //handle main nav - create tabs for each nav element
 
 projectView.handleMainNav = function () {
-
     $('.main-nav .tab').on('click', function () {
         event.preventDefault();
         var $whereToGo = $(this).data('content');
@@ -21,10 +20,31 @@ projectView.handleMainNav = function () {
     })
 };
 
+projectView.setTeasers = function () {
+    $('.project-body *:nth-of-type(n+2)').hide();
+
+    $('.read-on').on('click', function () {
+        event.preventDefault();
+        $(this).toggleClass('hidden');
+        $(this).parent().find($('.show-less')).toggleClass('hidden');
+        $(this).parent().find($('.project-body *:nth-of-type(n+2)')).fadeIn(750);
+    });
+
+    $('.show-less').on('click', function () {
+        event.preventDefault();
+        $(this).toggleClass('hidden');
+        $(this).parent().find($('.read-on')).toggleClass('hidden');
+        $(this).parent().find($('.project-body *:nth-of-type(n+2)')).hide();
+
+    });
+
+};
+
 
 
 //run all functions when DOC is ready
 $(document).ready(function () {
     projectView.handleMainNav();
+    projectView.setTeasers();
 
 })
