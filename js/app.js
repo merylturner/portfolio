c6986285c2f87a667a04ba04ca3c53057c82821f
Meryl Turner <merylturner08@gmail.com>
2017-05-31 17:10:11 -0700

    updated index.html and app.js file to show projects on page

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 'use strict';
 
 //TODO empty array?
+var allProjects = [];
 
 //TODO constructor function of projects
+function Project(projectDataObj) {
+	this.name = projectDataObj.name;
+	this.date = projectDataObj.date;
+	this.description = projectDataObj.description;
+	this.filepath = 'imgs/' + projectDataObj.filepath;
+}
 //TODO prototype function that will add data to the DOM with .toHtml()
+Project.prototype.toHtml = function () {
+    //TODO clone example markup for each project, using jQuery
+	var $newProject = $('article.template').clone();
+	$newProject.removeClass('template');
 
-//create variable grabbing the selector within the main section (div with id of #projectBlock)
-//TODO clone example markup for each project, using jQuery
+    //create variable grabbing the selector within the main section
+	$newProject.find('img').attr('src',this.filepath);
+	$newProject.find('h2').html(this.name);
+	$newProject.find('h4').html(this.date);
+	$newProject.find('p').html(this.description);
+	return $newProject;
 
+};
 
+//push each new project to the allProjects array
 
-
-
-//projectData.sort
-//projectData.forEach - push to the empty array
+projectData.forEach(function (projectDataObj) {
+	allProjects.push(new Project(projectDataObj));
+    console.log(allProjects);
+});
 //projectData.forEach - append items to the DOM via toHtml
+allProjects.forEach(function (project) {
+	$('#projects').append(project.toHtml());
 
+});
 
 
 //TODO create Meryl object with properties
--- js/projects.js
+++ js/projects.js
@@ -5,8 +5,7 @@ var projectData = [
         name:'Salmon Cookies',
         date:'May 2017',
         description:'<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>',
-        imageDesktop:'imgs/',
-        imageMobile:'imgs/',
+        filepath:'salmoncookies.png',
 
     },
 
@@ -14,8 +13,7 @@ var projectData = [
         name:'BusMall',
         date:'May 2017',
         description:'<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>',
-        imageDesktop:'imgs/',
-        imageMobile:'imgs/',
+        filepath:'busmall.png',
 
     },
 
@@ -23,8 +21,7 @@ var projectData = [
         name:'PocketCode',
         date:'May 2017',
         description:'<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>',
-        imageDesktop:'imgs/',
-        imageMobile:'imgs/',
+        filepath:'finalproject.png'
 
     },
 
