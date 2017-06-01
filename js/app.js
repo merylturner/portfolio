4d1c449f4c999d79523fb9d531bbd5334d300fba
Meryl Turner <merylturner08@gmail.com>
2017-05-31 17:39:22 -0700

    articles appear on page, hid the template article

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
 'use strict';
 
-//TODO empty array?
+//TODO empty array
 var allProjects = [];
 
 //TODO constructor function of projects
 function Project(projectDataObj) {
-	this.name = projectDataObj.name;
-	this.date = projectDataObj.date;
-	this.description = projectDataObj.description;
-	this.filepath = 'imgs/' + projectDataObj.filepath;
+    this.name = projectDataObj.name;
+    this.date = projectDataObj.date;
+    this.description = projectDataObj.description;
+    this.filepath = 'imgs/' + projectDataObj.filepath;
 }
 //TODO prototype function that will add data to the DOM with .toHtml()
 Project.prototype.toHtml = function () {
     //TODO clone example markup for each project, using jQuery
-	var $newProject = $('article.template').clone();
-	$newProject.removeClass('template');
+    var $newProject = $('article.template').clone();
+    $newProject.removeClass('template');
 
     //create variable grabbing the selector within the main section
-	$newProject.find('img').attr('src',this.filepath);
-	$newProject.find('h2').html(this.name);
-	$newProject.find('h4').html(this.date);
-	$newProject.find('p').html(this.description);
-	return $newProject;
+    $newProject.find('img').attr('src', this.filepath);
+    $newProject.find('h2').html(this.name);
+    $newProject.find('h4').html(this.date);
+    $newProject.find('p').html(this.description);
+    return $newProject;
 
 };
 
 //push each new project to the allProjects array
-
 projectData.forEach(function (projectDataObj) {
-	allProjects.push(new Project(projectDataObj));
-    console.log(allProjects);
+    allProjects.push(new Project(projectDataObj));
 });
+
 //projectData.forEach - append items to the DOM via toHtml
 allProjects.forEach(function (project) {
-	$('#projects').append(project.toHtml());
+    $('#projects').append(project.toHtml());
 
 });
 
+//hide template article
+$('.template').hide();
 
-//TODO create Meryl object with properties
