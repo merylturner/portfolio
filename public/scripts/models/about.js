00c088121ff72f2b2699db3a11496ed492ddce98
Meryl Turner <merylturner08@gmail.com>
2017-06-15 20:36:32 -0700

    fixed about.js to setup API endpoint

--- public/scripts/models/about.js
+++ public/scripts/models/about.js
@@ -8,16 +8,16 @@ var app = app || {};
 
     user.requestUser = function (callback) {
         if (!user.data) {
-            $.get('github/user');
-            //     $.ajax({
-            //         url: 'https://api.github.com/user',
-            //         type: 'GET',
-            //         headers: { 'Authorization': `token ${githubToken}` }  //eslint-disable-line
-            //     })
-            //         .then(data => user.data = data, err => console.error(err))
-            //         .then(callback);
-            // }
+            $.get('github/user')
+                .then(data => user.data = data, err => console.error(err))
+                .then(callback);
         }
+        //     $.ajax({
+        //         url: 'https://api.github.com/user',
+        //         type: 'GET',
+        //         headers: { 'Authorization': `token ${githubToken}` }  //eslint-disable-line
+        //     })
+
 
         module.user = user;
     }
